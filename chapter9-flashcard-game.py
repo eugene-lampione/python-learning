83245b896009c44ddd8cf2dbac9d5d9c5cd84bdd
Eugene Lampione <eugene.lampione@Mac.fios-router.home>
2026-03-05 19:56:22 -0500

    update flash card game

--- chapter9-flashcard-game.py
+++ chapter9-flashcard-game.py
@@ -5,142 +5,45 @@ from os import system
 def startGame():
 	system("clear") # clear the screen
 	print("Welcome to Math Flashcards!")
-	choice = input(
-		"Choose your flashcards (add | subtract | multiply | divide): ")
+	choices = ["+", "-", "*", "/"]
+	#choice = input(
+	#	"Choose your flashcards (add | subtract | multiply | divide): ")
 	numCards = input(
 		"How many Cards do you want to play? ")
+	showCards(int(numCards),choices)
 
-	selectCards(int(numCards),choice)
-
-def selectCards(numCards,choice):
-	if choice.lower() == "add":
-		additionCards(numCards)
-	elif choice.lower() == "subtract":
-		subtractionCards(numCards)
-	elif choice.lower() == "multiply":
-		multiplicationCards(numCards)
-	elif choice.lower() == "divide":
-		divisionCards(numCards)
-	else:
-		print(f"Invalid choice: {choice}")
-		input("Please hit enter to start over")
-		startGame()
-
-'''
-def playAgain(cards="", correct=0, incorrect=0):
-	playAgain = input("Would you like to play again? (yes|no|restart) ")
-
-	if playAgain.lower() == "yes":
-		selectCards(cards,correct,incorrect)
-	elif playAgain.lower() == "no":
-		print(f"Thanks for playing!")
-		print(f"You got {correct} correct.")
-		print(f"You got {incorrect} wrong.")
-	elif playAgain.lower() == "restart":		
-		print(f"Thanks for playing!")
-		print(f"You got {correct} correct.")
-		print(f"You got {incorrect} wrong.")
-		input("Hit enter to restart")
-		startGame()
-	else:
-		print(f"Sorry didn't recognize {playAgain}")
-		playAgain()
-'''
-
-def additionCards(numCards):
+def showCards(numCards,choices):
 	correctAns = 0
 	incorrectAns = 0
 	cardsPlayed = 1
 	while cardsPlayed <= numCards:
-		number1 = random.randint(0,10)
-		number2 = random.randint(0,10)
-		solution = number1 + number2
-		answer = input(f"{cardsPlayed}. {number1} + {number2} = ")
-
-		if int(answer) == int(solution):
-			correctAns += 1
-			print(f"CORRECT: {number1} + {number2} = {solution}")
-		else:
-			incorrectAns += 1
-			print(f"INCORRECT: {number1} + {number2} = {solution}")
-
-		cardsPlayed +=1
-
-	print(f"You got {correctAns} correct and {incorrectAns} wrong")
-	playAgain = input("Play Again? ")
-	if(playAgain.lower() == 'yes'):
-		startGame()
-	else:
-		print("Thanks for playing.")
-
-def subtractionCards(numCards):
-	correctAns = 0
-	incorrectAns = 0
-	cardsPlayed = 1
-	while cardsPlayed <= numCards:
-		number1 = random.randint(0,10)
-		number2 = random.randint(0,10)
-		solution = number1 - number2
-		answer = input(f"{cardsPlayed}. {number1} - {number2} = ")
-
-		if int(answer) == int(solution):
-			correctAns += 1
-			print(f"CORRECT: {number1} - {number2} = {solution}")
-		else:
-			incorrectAns += 1
-			print(f"INCORRECT: {number1} - {number2} = {solution}")
-
-		cardsPlayed +=1
-
-	print(f"You got {correctAns} correct and {incorrectAns} wrong")
-	playAgain = input("Play Again? ")
-	if(playAgain.lower() == 'yes'):
-		startGame()
-	else:
-		print("Thanks for playing.")
-
-def multiplicationCards(numCards):
-	correctAns = 0
-	incorrectAns = 0
-	cardsPlayed = 1
-	while cardsPlayed <= numCards:
-		number1 = random.randint(0,10)
-		number2 = random.randint(0,10)
-		solution = number1 * number2
-		answer = input(f"{cardsPlayed}. {number1} * {number2} = ")
-
-		if int(answer) == int(solution):
-			correctAns += 1
-			print(f"CORRECT: {number1} * {number2} = {solution}")
-		else:
-			incorrectAns += 1
-			print(f"INCORRECT: {number1} * {number2} = {solution}")
-
-		cardsPlayed +=1
-	
-	print(f"You got {correctAns} correct and {incorrectAns} wrong")
-	playAgain = input("Play Again? ")
-	if(playAgain.lower() == 'yes'):
-		startGame()
-	else:
-		print("Thanks for playing.")
-
-def divisionCards(numCards):
-	correctAns = 0
-	incorrectAns = 0
-	cardsPlayed = 1
-	while cardsPlayed <= numCards:
+		choiceIndex = random.randint(0,3)
+		operation = choices[choiceIndex]
 		number1 = random.randint(0,10)
 		number2 = random.randint(1,10)
-		solution = float(number1 / number2)
-		answer = input(f"{cardsPlayed}. {number1} / {number2} = ")
+		if operation == "+":
+			solution = int(number1 + number2)
+		elif operation == "-":
+			solution = int(number1 - number2)
+		elif operation == "*":
+			solution = int(number1 * number2)
+		elif operation == "/":
+			solution = float(number1 / number2)
+			divmodSolution = divmod(number1, number2)
+			print(divmodSolution)
 
-		if float(answer) == solution:
+		answer = input(f"{cardsPlayed}. {number1} {operation} {number2} = ")
+		if operation == "/":
+			answer = float(answer)
+		else:
+			answer = int(answer)
+
+		if answer == solution:
 			correctAns += 1
-			print(f"CORRECT: {number1} / {number2} = {solution}")
+			print(f"CORRECT: {number1} {operation} {number2} = {solution}")
 		else:
 			incorrectAns += 1
-			print(f"INCORRECT: {number1} / {number2} = {solution}")
+			print(f"INCORRECT: {number1} {operation} {number2} = {solution}")
 
 		cardsPlayed +=1
 
@@ -150,7 +53,6 @@ def divisionCards(numCards):
 		startGame()
 	else:
 		print("Thanks for playing.")
-
 
 startGame()
 
